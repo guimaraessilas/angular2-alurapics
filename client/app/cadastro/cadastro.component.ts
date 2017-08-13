a8611693e927268113fe60c2c734f19ea95c3103
Silas Guimarães Ribeiro <guimaraes.silas@hotmail.com>
2017-08-13 14:33:23 -0300

    configuração de rotas

---
+++ client/app/cadastro/cadastro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'cadastro',
+    moduleId: module.id,
+    templateUrl: './cadastro.component.html'    
+})
+export class CadastroComponent{
+    
+}
