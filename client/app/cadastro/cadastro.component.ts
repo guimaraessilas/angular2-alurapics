e14b2a6471542d714e92b54a12cadfe617f850d7
Silas Guimarães Ribeiro <guimaraes.silas@hotmail.com>
2017-08-15 13:31:04 -0300

    formulario de cadastro

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FotoComponent } from "../foto/foto.component";
 
 @Component({
     selector:'cadastro',
@@ -7,4 +8,9 @@ import { Component } from "@angular/core";
 })
 export class CadastroComponent{
     
+    foto: FotoComponent = new FotoComponent();
+
+    cadastrar(){
+        
+    }
 }
